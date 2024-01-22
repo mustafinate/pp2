f745fb8269b88c05ec23d8b278b78502976fa54a
mustafinate <tomirismustafina20@gmail.com>
2024-01-22 13:09:13 +0600

    First commit

---
+++ python/lab1/lab1/datatypes/type1.py
@@ -0,0 +1,49 @@
+#ex1
+x = 5
+print(type(x))
+int
+
+#output <class 'int'>
+
+
+#ex2
+x = "Hello World"
+print(type(x))
+str
+
+#output <class 'str'>
+
+#ex3
+x = 20.5
+print(type(x))
+float
+
+#output <class 'float'>
+
+#ex4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+list
+
+#output  <class 'list'>
+
+#ex5
+x = ("apple", "banana", "cherry")
+print(type(x))
+tuple
+
+#output <class 'tuple'>
+
+#ex6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+dict
+
+#output <class 'dict'>
+
+#ex7
+x = True
+print(type(x))
+bool
+
+#output <class 'bool'>
---
+++ python/lab1/lab1/syntax/exe1.py
@@ -0,0 +1,10 @@
+#ex1
+print("Hello, world!")
+
+#output Hello, world!
+
+#ex2
+if 5>2:
+    print("YES")
+    
+#output YES
